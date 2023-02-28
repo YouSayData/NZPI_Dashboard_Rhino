11ae3ed1b3b5bd3cda97d2db3e5f802fbd96b7b4
Thomas Koentges <thomas@yousaydata.com>
2023-03-01 11:17:46 +1300

    commit renv activation, tidy up db_functions.R

--- app/logic/db_functions.R
+++ app/logic/db_functions.R
@@ -1,40 +1,43 @@
 box::use(
-  shiny[reactive],
-  pool[dbPool],
-  RMariaDB,
-  DBI,
-  dbplyr,
-  dplyr,
-  tidyr[pivot_wider, tibble],
-  shiny[req, isolate],
+  DBI[
+    dbListTables,
+  ],
+  dplyr[
+    tbl,
+    collect,
+    semi_join,
+    filter,
+    select,
+    distinct,
+  ],
 )
 
 #' @export
 db_list_tables <- function(db_pool) {
-  DBI$dbListTables(db_pool, tbl_name)
+  dbListTables(db_pool, tbl_name)
 }
 
 #' @export
 db_collect <- function(tbl_name, db_pool) {
-  dplyr$tbl(db_pool, tbl_name) |>
-    dplyr$collect()
+  tbl(db_pool, tbl_name) |>
+    collect()
 }
 
 #' @export
 db_view <- function(tbl_name, db_pool) {
-  dplyr$tbl(db_pool, tbl_name)
+  tbl(db_pool, tbl_name)
 }
 
 #' @export
 db_get_nests_by_site <- function(.site_id, db_pool) {
   db_view("nest_id", db_pool) |>
-    dplyr$semi_join(db_view("nest_checks", db_pool) |>
-                      dplyr$select(id = nest_id, site) |> 
-                      dplyr$distinct()) |> 
-    dplyr$filter(site == .site_id) |>
-    dplyr$select(site, nest_id = id, nest_name = nest_id) |>
-    dplyr$distinct() |>
-    dplyr$collect()
+    semi_join(db_view("nest_checks", db_pool) |>
+                      select(id = nest_id, site) |> 
+                      distinct()) |> 
+    filter(site == .site_id) |>
+    select(site, nest_id = id, nest_name = nest_id) |>
+    distinct() |>
+    collect()
 }
 
 
